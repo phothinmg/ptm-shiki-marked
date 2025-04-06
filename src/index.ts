97bf84d1ce60215e8fc69097e591179c5d4d39ac
phothinmg <phothinmg@gmail.com>
2025-04-06 11:06:04 +0630

    modified:   package.json

--- src/index.ts
+++ src/index.ts
@@ -1,79 +1,31 @@
 import { MarkedExtension } from "marked";
-import * as shiki from "shiki";
 
-export type ShikiMarkedOptions = {
-  hightlighter: shiki.HighlighterGeneric<
-    shiki.BundledLanguage,
-    shiki.BundledTheme
-  >;
-  themes: {
-    light: shiki.BundledTheme;
-    dark: shiki.BundledTheme;
-  };
+type HighlightFunction = (code: string, language: any) => string;
+
+export type Options = {
+  shikiHighlight: HighlightFunction;
+  shikiTwoslash?: HighlightFunction;
+  colorizedBrackets?: HighlightFunction;
   codeContainer?: string;
-  transformers?: shiki.ShikiTransformer[];
-  twoslashTransformers?: shiki.ShikiTransformer[];
-  colorizedBracketsTransformers?: shiki.ShikiTransformer[];
 };
 
-/**
- * A marked extension for Shiki.js.
- *
- * @param {ShikiMarkedOptions} opts
- * @param {shiki.HighlighterGeneric<shiki.BundledLanguage,shiki.BundledTheme>} opts.hightlighter
- * @param {{light: shiki.BundledTheme, dark: shiki.BundledTheme}} opts.themes
- * @param {shiki.ShikiTransformer[]} opts.transformers
- * @param {shiki.ShikiTransformer[]} [opts.twoslashTransformers]
- * @param {shiki.ShikiTransformer[]} [opts.colorizedBracketsTransformers]
- * @param {string} [opts.codeContainer]
- * @returns {MarkedExtension}
- */
 export function shikiMarked({
-  hightlighter,
-  themes,
-  transformers,
-  twoslashTransformers,
-  colorizedBracketsTransformers,
+  shikiHighlight,
+  shikiTwoslash,
+  colorizedBrackets,
   codeContainer,
-}: ShikiMarkedOptions): MarkedExtension {
-  const shikiHighlight = (code: string, lang: shiki.BundledLanguage) => {
-    return hightlighter.codeToHtml(code, {
-      lang: lang,
-      themes: {
-        light: themes.light,
-        dark: themes.dark,
-      },
-      transformers: [...transformers],
-    });
-  };
-  const colorizedBrackets = (code: string, lang: shiki.BundledLanguage) => {
-    return hightlighter.codeToHtml(code, {
-      lang: lang,
-      themes: {
-        light: themes.light,
-        dark: themes.dark,
-      },
-      transformers: [...colorizedBracketsTransformers],
-    });
-  };
-  const shikiTwoslash = (code: string, lang: shiki.BundledLanguage) => {
-    return hightlighter.codeToHtml(code, {
-      lang: lang,
-      themes: {
-        light: themes.light,
-        dark: themes.dark,
-      },
-      transformers: [...twoslashTransformers],
-    });
-  };
+}: Options): MarkedExtension {
   return {
     walkTokens(token) {
       if (token.type === "code") {
         const lanTexts: any[] = token.lang.split(" ");
         const lang = lanTexts[0];
-        const isTwo = lanTexts.length > 1 && lanTexts.includes("twoslash");
+        const isTwo =
+          shikiTwoslash && lanTexts.length > 1 && lanTexts.includes("twoslash");
         const isBrackets =
-          lanTexts.length > 1 && lanTexts.includes("colorize-brackets");
+          colorizedBrackets &&
+          lanTexts.length > 1 &&
+          lanTexts.includes("colorize-brackets");
         const code = token.text;
         const highlighted = isTwo
           ? shikiTwoslash(code, lang)
